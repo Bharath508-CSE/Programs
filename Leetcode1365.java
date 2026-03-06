a2aa2bd4c11de6f12f570f135cf4d53be9c334cb
Bharath508-CSE <bharath23695a0508@gmail.com>
2026-03-06 23:04:36 +0530

    1365 problem

---
+++ Leetcode1365.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Leetcode1365
+{
+    public static void main(String[] args)
+    {
+        int[] arr={8,1,2,3,4};
+        smallerNumbersThanCurrent(arr);
+
+    }
+    public static void smallerNumbersThanCurrent(int[] nums)
+    {
+        int[] result=new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            int count=0;
+            for(int j=0;j<nums.length;j++)
+            {
+                if(nums[j]<nums[i])
+                {
+                    count+=1;
+                }
+            }
+            result[i]=count;
+        }
+        System.err.println(Arrays.toString(result));
+    }
+}
