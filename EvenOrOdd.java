ee79d1fea10c406a7f4cd8e1a93a9e81ebc29009
Bharath508-CSE <bharath23695a0508@gmail.com>
2026-03-23 19:31:10 +0530

    Tricky Question EvenOrOdd

---
+++ EvenOrOdd.java
@@ -0,0 +1,13 @@
+//Tricky question Evene or Odd
+public class EvenOrOdd {
+
+    public static void main(String[] args) {
+        int number = 5; // You can change this number to test with different values
+
+        if ((number & 1)== 0) {
+            System.out.println(number + " is Odd.");
+        } else {
+            System.out.println(number + " is Even.");
+        }
+    }
+}
