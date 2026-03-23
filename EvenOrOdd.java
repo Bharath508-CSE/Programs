3bd0f2e74f6dbbbefcadf7cdf8a0d117f138c974
Bharath508-CSE <bharath23695a0508@gmail.com>
2026-03-23 20:00:45 +0530

    Tricky Question EvenOrOdd Modified

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -4,10 +4,22 @@ public class EvenOrOdd {
     public static void main(String[] args) {
         int number = 5; // You can change this number to test with different values
 
-        if ((number & 1)== 0) {
-            System.out.println(number + " is Odd.");
-        } else {
-            System.out.println(number + " is Even.");
+        // if ((number & 1)== 0) {
+        //     System.out.println(number + " is Odd.");
+        // } else {
+        //     System.out.println(number + " is Even.");
+        // }
+
+        if(isEven(number))
+        {
+            System.out.println("Is Even");
         }
+        else
+        {
+            System.out.println("Is Odd");
+        }
+    }
+    static boolean isEven(int number) {
+        return (number & 1) == 0;
     }
 }
