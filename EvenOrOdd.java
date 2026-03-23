81795ee399b0684dd51a8135bf357c7d0ec33d9f
Bharath <bharath23695a0508@gmail.com>
2026-03-23 19:41:37 +0530

    Clarify bitwise operation in EvenOrOdd.java
    Updated the comment to explain the use of the bitwise AND operator.

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -4,7 +4,7 @@ public class EvenOrOdd {
     public static void main(String[] args) {
         int number = 5; // You can change this number to test with different values
 
-        if ((number & 1)== 0) {
+        if ((number & 1)== 0) {   //Here i used Bitwise and operator if both bits are equal it will return 1 other wise it will return 0
             System.out.println(number + " is Odd.");
         } else {
             System.out.println(number + " is Even.");
